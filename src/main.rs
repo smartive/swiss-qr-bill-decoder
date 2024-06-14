cf63411f8e7d27b9d47446237dafc9281da24a1e
mike <mike@smartive.ch>
2024-06-14 13:46:14 +0200

    parse pdf

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+use std::path::Path;
 use image;
 use rqrr::PreparedImage;
-use crate::models::address::Address;
-use crate::models::qr_data::QRData;
+use std::process::{Command, Stdio};
 
 mod models;
+mod qr_parser;
 
 fn main() {
-    let file_name = "test_image.png";
+    let file_input = "data/minimal.pdf";
 
-    let img = image::open(file_name).expect("Error loading image").to_luma8();
+    // use ghostscript to convert the PDF to a PNG
+    let png = gs_command(file_input)
+        .output()
+        .expect("Error running Ghostscript. Is 'gs' installed?");
+
+    // read image from memory
+    let img = image::load_from_memory_with_format(&png.stdout, image::ImageFormat::Png)
+        .expect("Error loading image").to_luma8();
 
     let mut img = PreparedImage::prepare(img);
 
@@ -26,78 +34,30 @@ fn main() {
         .collect();
 
     for text_data in &qr_codes {
-        let qr_data = get_qr_code_data(text_data);
+        let qr_data = qr_parser::get_qr_code_data(text_data);
         println!("{:?}", qr_data);
     }
 }
 
-fn get_qr_code_data(text: &String) -> QRData {
-    for (i, line) in text.lines().enumerate() {
-        println!("[{i}] {line}");
-    }
-
-    let mut lines = text.lines();
-    assert_eq!(lines.next(), Some("SPC"), "First line is not 'SPC'");
-    assert_eq!(lines.next(), Some("0200"), "Only version 0200 is supported");
-    assert_eq!(lines.next(), Some("1"), "Only coding type 1 (UTF-8) is supported");
-
-    let iban = lines.next().expect("Missing IBAN");
-    assert!(iban.starts_with("CH") || iban.starts_with("LI"), "Only CH and LI IBANs are supported");
-
-    let recipient_address = Address::new(
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-    );
-
-    // skip 7 lines
-    for _ in 0..7 {
-        let _ = lines.next();
-    }
-
-    let amount = lines.next().expect("Missing amount");
-
-    let currency = lines.next().expect("Missing currency");
-    assert!(currency.eq("CHF") || currency.eq("EUR"), "Only CHF and EUR currencies are supported");
-
-    let address_type = lines.next().expect("Missing address type");
-    let sender_address = if !address_type.is_empty() {
-        Some(Address::new(
-            Some(address_type),
-            lines.next(),
-            lines.next(),
-            lines.next(),
-            lines.next(),
-            lines.next(),
-            lines.next(),
-        ))
-    } else {
-        None
-    };
-
-    let reference_type = lines.next().expect("Missing reference type");
-    assert!(reference_type.eq("NON") || reference_type.eq("QRR") || reference_type.eq("SCOR"), "Only reference types NON, QRR and SCOR are supported");
-
-    let reference = lines.next().expect("Missing reference");
-    assert!((reference_type.eq("QRR") || reference_type.eq("SCOR")) && !reference.is_empty(), "Reference is empty");
-
-    let message = lines.next().expect("Missing message");
-
-    assert_eq!(lines.next(), Some("EPD"), "Missing trailing 'EPD'");
-
-    let qr_data = QRData::new(
-        iban.to_string(),
-        recipient_address,
-        sender_address,
-        amount.to_string(),
-        currency.to_string(),
-        reference_type.to_string(),
-        reference.to_string(),
-        message.to_string(),
-    );
-    qr_data
+pub fn gs_command<P>(in_path: P) -> Command
+    where
+        P: AsRef<Path>,
+{
+    let mut cmd = Command::new("gs");
+    cmd.args(
+        [
+            "-q",
+            "-dBATCH",
+            "-dSAFER",
+            "-dNOPAUSE",
+            "-dFILTERTEXT",
+            "-r300",
+            "-sDEVICE=pngmono",
+            "-sOutputFile=-"
+        ]
+            .iter(),
+    )
+        .arg(in_path.as_ref().to_string_lossy().to_string())
+        .stdout(Stdio::piped());
+    cmd
 }
---
+++ src/qr_parser.rs
@@ -0,0 +1,79 @@
+use crate::models::address::Address;
+use crate::models::qr_data::QRData;
+
+pub fn get_qr_code_data(text: &String) -> QRData {
+    for (i, line) in text.lines().enumerate() {
+        println!("[{i}] {line}");
+    }
+
+    let mut lines = text.lines();
+    assert_eq!(lines.next(), Some("SPC"), "First line is not 'SPC'");
+    assert_eq!(lines.next(), Some("0200"), "Only version 0200 is supported");
+    assert_eq!(lines.next(), Some("1"), "Only coding type 1 (UTF-8) is supported");
+
+    let iban = lines.next().expect("Missing IBAN");
+    assert!(iban.starts_with("CH") || iban.starts_with("LI"), "Only CH and LI IBANs are supported");
+
+    let recipient_address = Address::new(
+        lines.next(),
+        lines.next(),
+        lines.next(),
+        lines.next(),
+        lines.next(),
+        lines.next(),
+        lines.next(),
+    );
+
+    // skip 7 lines
+    for _ in 0..7 {
+        let _ = lines.next();
+    }
+
+    let amount = lines.next().expect("Missing amount");
+
+    let currency = lines.next().expect("Missing currency");
+    assert!(currency.eq("CHF") || currency.eq("EUR"), "Only CHF and EUR currencies are supported");
+
+    let address_type = lines.next().expect("Missing address type");
+    let sender_address = if !address_type.is_empty() {
+        Some(Address::new(
+            Some(address_type),
+            lines.next(),
+            lines.next(),
+            lines.next(),
+            lines.next(),
+            lines.next(),
+            lines.next(),
+        ))
+    } else {
+        // skip 7 lines
+        for _ in 0..6 {
+            let _ = lines.next();
+        }
+        None
+    };
+
+    let reference_type = lines.next().expect("Missing reference type");
+    assert!(reference_type.eq("NON") || reference_type.eq("QRR") || reference_type.eq("SCOR"), "Only reference types NON, QRR and SCOR are supported");
+
+    let reference = lines.next().expect("Missing reference");
+    if reference_type.eq("QRR") || reference_type.eq("SCOR") {
+        assert!(!reference.is_empty(), "Reference is empty");
+    }
+
+    let message = lines.next().expect("Missing message");
+
+    assert_eq!(lines.next(), Some("EPD"), "Missing trailing 'EPD'");
+
+    let qr_data = QRData::new(
+        iban.to_string(),
+        recipient_address,
+        sender_address,
+        amount.to_string(),
+        currency.to_string(),
+        reference_type.to_string(),
+        reference.to_string(),
+        message.to_string(),
+    );
+    qr_data
+}
