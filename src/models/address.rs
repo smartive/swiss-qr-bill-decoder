3bbfe24e5fdbca9ed5da64b4f93d5082dba6040b
mike <mike@smartive.ch>
2024-06-18 14:21:51 +0200

    cleanup code

--- src/models/address.rs
+++ src/models/address.rs
@@ -12,76 +12,18 @@ pub struct Address {
 
 impl Address {
     pub(crate) fn new(
-        address_type: Option<&str>,
-        name: Option<&str>,
-        street_or_address_line_1: Option<&str>,
-        building_number_or_address_line_2: Option<&str>,
-        postal_code: Option<&str>,
-        town: Option<&str>,
-        country: Option<&str>,
-    ) -> Result<Self, String> {
-        let address_type = match address_type {
-            None => return Err("Missing address type".to_string()),
-            Some(address_type) if address_type.is_empty() => {
-                return Err("Address type is empty".to_string())
-            }
-            Some(address_type) if !address_type.eq("K") && !address_type.eq("S") => {
-                return Err("Only address types K and S are supported".to_string())
-            }
-            Some(address_type) => address_type.to_string(),
-        };
-
-        let name = match name {
-            None => return Err("Missing name".to_string()),
-            Some(name) if name.is_empty() => return Err("Recipient name is empty".to_string()),
-            Some(name) => name.to_string(),
-        };
-
-        let street_or_address_line_1 = match street_or_address_line_1 {
-            None => return Err("Missing street or address line 1".to_string()),
-            Some(street_or_address_line_1) => street_or_address_line_1.to_string(),
-        };
-
-        let building_number_or_address_line_2 = match building_number_or_address_line_2 {
-            None => return Err("Missing building number or address line 2".to_string()),
-            Some(building_number_or_address_line_2) => {
-                building_number_or_address_line_2.to_string()
-            }
-        };
-
-        let postal_code = match postal_code {
-            None => return Err("Missing postal code".to_string()),
-            Some(postal_code) => postal_code.to_string(),
-        };
-
-        let town = match town {
-            None => return Err("Missing town".to_string()),
-            Some(town) => town.to_string(),
-        };
-
-        let country = match country {
-            None => return Err("Missing country".to_string()),
-            Some(country) => country.to_string(),
-        };
-
-        let address_line_1 = if address_type.eq("K") {
-            street_or_address_line_1.to_string()
-        } else {
-            format!("{street_or_address_line_1} {building_number_or_address_line_2}")
-        };
-
-        let address_line_2 = if address_type.eq("K") {
-            building_number_or_address_line_2.to_string()
-        } else {
-            format!("{postal_code} {town}")
-        };
-
-        Ok(Self {
+        address_type: String,
+        name: String,
+        address_line_1: String,
+        address_line_2: String,
+        country: String,
+    ) -> Self {
+        Self {
             address_type,
             name,
             address_line_1,
             address_line_2,
             country,
-        })
+        }
     }
 }
--- src/qr_parser.rs
+++ src/qr_parser.rs
@@ -114,15 +114,64 @@ fn skip_lines(lines: &mut Lines, skip_lines: i32) {
 }
 
 fn to_address(lines: &mut Lines, address_type: &str) -> Result<Address, String> {
-    Address::new(
-        Some(address_type),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-        lines.next(),
-    )
+    let address_type = match address_type {
+        address_type if address_type.is_empty() => return Err("Address type is empty".to_string()),
+        address_type if !address_type.eq("K") && !address_type.eq("S") => {
+            return Err("Only address types K and S are supported".to_string())
+        }
+        address_type => address_type.to_string(),
+    };
+
+    let name = match lines.next() {
+        None => return Err("Missing name".to_string()),
+        Some(name) if name.is_empty() => return Err("Recipient name is empty".to_string()),
+        Some(name) => name.to_string(),
+    };
+
+    let street_or_address_line_1 = match lines.next() {
+        None => return Err("Missing street or address line 1".to_string()),
+        Some(street_or_address_line_1) => street_or_address_line_1.to_string(),
+    };
+
+    let building_number_or_address_line_2 = match lines.next() {
+        None => return Err("Missing building number or address line 2".to_string()),
+        Some(building_number_or_address_line_2) => building_number_or_address_line_2.to_string(),
+    };
+
+    let postal_code = match lines.next() {
+        None => return Err("Missing postal code".to_string()),
+        Some(postal_code) => postal_code.to_string(),
+    };
+
+    let town = match lines.next() {
+        None => return Err("Missing town".to_string()),
+        Some(town) => town.to_string(),
+    };
+
+    let country = match lines.next() {
+        None => return Err("Missing country".to_string()),
+        Some(country) => country.to_string(),
+    };
+
+    let address_line_1 = if address_type.eq("K") {
+        street_or_address_line_1.to_string()
+    } else {
+        format!("{street_or_address_line_1} {building_number_or_address_line_2}")
+    };
+
+    let address_line_2 = if address_type.eq("K") {
+        building_number_or_address_line_2.to_string()
+    } else {
+        format!("{postal_code} {town}")
+    };
+
+    Ok(Address::new(
+        address_type,
+        name,
+        address_line_1,
+        address_line_2,
+        country,
+    ))
 }
 
 #[cfg(test)]
@@ -173,27 +222,19 @@ mod tests {
         let expected = QRData::new(
             "CH6431961000004421557".to_string(),
             Address::new(
-                Some("S"),
-                Some("Max Muster & Söhne"),
-                Some("Musterstrasse"),
-                Some("123"),
-                Some("8000"),
-                Some("Seldwyla"),
-                Some("CH"),
-            )
-            .unwrap(),
-            Some(
-                Address::new(
-                    Some("S"),
-                    Some("Simon Muster"),
-                    Some("Musterstrasse"),
-                    Some("1"),
-                    Some("8000"),
-                    Some("Seldwyla"),
-                    Some("CH"),
-                )
-                .unwrap(),
+                "S".to_string(),
+                "Max Muster & Söhne".to_string(),
+                "Musterstrasse 123".to_string(),
+                "8000 Seldwyla".to_string(),
+                "CH".to_string(),
             ),
+            Some(Address::new(
+                "S".to_string(),
+                "Simon Muster".to_string(),
+                "Musterstrasse 1".to_string(),
+                "8000 Seldwyla".to_string(),
+                "CH".to_string(),
+            )),
             Some("50.00".to_string()),
             "CHF".to_string(),
             "QRR".to_string(),
