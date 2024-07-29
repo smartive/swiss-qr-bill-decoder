dc7805cefdd8c33f8a83fbace307f9cc6bb03c32
mike <mike@smartive.ch>
2024-07-29 15:42:25 +0200

    feat: add support for jpg/jpeg images

--- tests/edge_cases.rs
+++ tests/edge_cases.rs
@@ -6,25 +6,24 @@ use swiss_qr_bill_decoder::models::qr_data::QRData;
 fn minimal_png() {
     let actual = get_qr_bill_data("tests/data/minimal.png".to_string(), true);
 
-    let expected = vec![QRData::new(
-        "CH4289144165265158476".to_string(),
-        Address::new(
-            "S".to_string(),
-            "A".to_string(),
-            " ".to_string(),
-            "8000 Zürich".to_string(),
-            "CH".to_string(),
-        ),
-        None,
-        None,
-        "CHF".to_string(),
-        "NON".to_string(),
-        None,
-        None,
-    )];
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_minimal());
+}
 
-    assert_eq!(actual.len(), expected.len());
-    assert_eq!(actual[0], expected[0]);
+#[test]
+fn minimal_jpg() {
+    let actual = get_qr_bill_data("tests/data/minimal.jpg".to_string(), true);
+
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_minimal());
+}
+
+#[test]
+fn minimal_jpeg() {
+    let actual = get_qr_bill_data("tests/data/minimal.jpeg".to_string(), true);
+
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_minimal());
 }
 
 #[test]
@@ -32,56 +31,32 @@ fn minimal_png() {
 fn minimal_pdf() {
     let actual = get_qr_bill_data("tests/data/minimal.pdf".to_string(), true);
 
-    let expected = vec![QRData::new(
-        "CH4289144165265158476".to_string(),
-        Address::new(
-            "S".to_string(),
-            "A".to_string(),
-            " ".to_string(),
-            "8000 Zürich".to_string(),
-            "CH".to_string(),
-        ),
-        None,
-        None,
-        "CHF".to_string(),
-        "NON".to_string(),
-        None,
-        None,
-    )];
-
-    assert_eq!(actual.len(), expected.len());
-    assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_minimal());
 }
 
 #[test]
 fn full_png() {
     let actual = get_qr_bill_data("tests/data/full.png".to_string(), true);
 
-    let expected = vec![QRData::new(
-        "CH3389144927977473182".to_string(),
-        Address::new(
-            "S".to_string(),
-            "Test Recipient AG".to_string(),
-            "Teststreet 42a".to_string(),
-            "9000 Zürich".to_string(),
-            "CH".to_string(),
-        ),
-        Some(Address::new(
-            "S".to_string(),
-            "Sender AG".to_string(),
-            "Senderstreet 99C".to_string(),
-            "1234 Sendertown".to_string(),
-            "AT".to_string(),
-        )),
-        Some("1337.42".to_string()),
-        "EUR".to_string(),
-        "SCOR".to_string(),
-        Some("RF541234".to_string()),
-        Some("This is a test Message".to_string()),
-    )];
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_full());
+}
 
-    assert_eq!(actual.len(), expected.len());
-    assert_eq!(actual[0], expected[0]);
+#[test]
+fn full_jpg() {
+    let actual = get_qr_bill_data("tests/data/full.jpg".to_string(), true);
+
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_full());
+}
+
+#[test]
+fn full_jpeg() {
+    let actual = get_qr_bill_data("tests/data/full.jpeg".to_string(), true);
+
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_full());
 }
 
 #[test]
@@ -89,62 +64,32 @@ fn full_png() {
 fn full_pdf() {
     let actual = get_qr_bill_data("tests/data/full.pdf".to_string(), true);
 
-    let expected = vec![QRData::new(
-        "CH3389144927977473182".to_string(),
-        Address::new(
-            "S".to_string(),
-            "Test Recipient AG".to_string(),
-            "Teststreet 42a".to_string(),
-            "9000 Zürich".to_string(),
-            "CH".to_string(),
-        ),
-        Some(Address::new(
-            "S".to_string(),
-            "Sender AG".to_string(),
-            "Senderstreet 99C".to_string(),
-            "1234 Sendertown".to_string(),
-            "AT".to_string(),
-        )),
-        Some("1337.42".to_string()),
-        "EUR".to_string(),
-        "SCOR".to_string(),
-        Some("RF541234".to_string()),
-        Some("This is a test Message".to_string()),
-    )];
-
-    assert_eq!(actual.len(), expected.len());
-    assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_full());
 }
 
 #[test]
 fn rotated_png() {
     let actual = get_qr_bill_data("tests/data/rotated.png".to_string(), true);
 
-    let expected = vec![QRData::new(
-        "CH3389144927977473182".to_string(),
-        Address::new(
-            "S".to_string(),
-            "Test Recipient AG".to_string(),
-            "Teststreet 42a".to_string(),
-            "9000 Zürich".to_string(),
-            "CH".to_string(),
-        ),
-        Some(Address::new(
-            "S".to_string(),
-            "Sender AG".to_string(),
-            "Senderstreet 99C".to_string(),
-            "1234 Sendertown".to_string(),
-            "AT".to_string(),
-        )),
-        Some("1337.42".to_string()),
-        "EUR".to_string(),
-        "SCOR".to_string(),
-        Some("RF541234".to_string()),
-        Some("This is a test Message".to_string()),
-    )];
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_rotated());
+}
 
-    assert_eq!(actual.len(), expected.len());
-    assert_eq!(actual[0], expected[0]);
+#[test]
+fn rotated_jpg() {
+    let actual = get_qr_bill_data("tests/data/rotated.jpg".to_string(), true);
+
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_rotated());
+}
+
+#[test]
+fn rotated_jpeg() {
+    let actual = get_qr_bill_data("tests/data/rotated.jpeg".to_string(), true);
+
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_rotated());
 }
 
 #[test]
@@ -152,31 +97,8 @@ fn rotated_png() {
 fn rotated_pdf() {
     let actual = get_qr_bill_data("tests/data/rotated.pdf".to_string(), true);
 
-    let expected = vec![QRData::new(
-        "CH3389144927977473182".to_string(),
-        Address::new(
-            "S".to_string(),
-            "Test Recipient AG".to_string(),
-            "Teststreet 42a".to_string(),
-            "9000 Zürich".to_string(),
-            "CH".to_string(),
-        ),
-        Some(Address::new(
-            "S".to_string(),
-            "Sender AG".to_string(),
-            "Senderstreet 99C".to_string(),
-            "1234 Sendertown".to_string(),
-            "AT".to_string(),
-        )),
-        Some("1337.42".to_string()),
-        "EUR".to_string(),
-        "SCOR".to_string(),
-        Some("RF541234".to_string()),
-        Some("This is a test Message".to_string()),
-    )];
-
-    assert_eq!(actual.len(), expected.len());
-    assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0], expected_rotated());
 }
 
 #[test]
@@ -184,99 +106,55 @@ fn double_png() {
     let actual = get_qr_bill_data("tests/data/double.png".to_string(), true);
 
     let expected = vec![
-        QRData::new(
-            "CH3389144927977473182".to_string(),
-            Address::new(
-                "S".to_string(),
-                "Test Recipient AG".to_string(),
-                "Teststreet 42a".to_string(),
-                "9000 Zürich".to_string(),
-                "CH".to_string(),
-            ),
-            Some(Address::new(
-                "S".to_string(),
-                "Sender AG".to_string(),
-                "Senderstreet 99C".to_string(),
-                "1234 Sendertown".to_string(),
-                "AT".to_string(),
-            )),
-            Some("1337.42".to_string()),
-            "EUR".to_string(),
-            "SCOR".to_string(),
-            Some("RF541234".to_string()),
-            Some("This is a test Message".to_string()),
-        ),
-        QRData::new(
-            "CH4289144165265158476".to_string(),
-            Address::new(
-                "S".to_string(),
-                "A".to_string(),
-                " ".to_string(),
-                "8000 Zürich".to_string(),
-                "CH".to_string(),
-            ),
-            None,
-            None,
-            "CHF".to_string(),
-            "NON".to_string(),
-            None,
-            None,
-        ),
+        expected_double_1(),
+        expected_double_2(),
     ];
 
     assert_eq!(actual.len(), expected.len());
     assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual[1], expected[1]);
 }
 
+#[test]
+fn double_jpg() {
+    let actual = get_qr_bill_data("tests/data/double.jpg".to_string(), true);
+
+    let expected = vec![
+        expected_double_1(),
+        expected_double_2(),
+    ];
+
+    assert_eq!(actual.len(), expected.len());
+    assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual[1], expected[1]);
+}
+
+#[test]
+fn double_jpeg() {
+    let actual = get_qr_bill_data("tests/data/double.jpeg".to_string(), true);
+
+    let expected = vec![
+        expected_double_1(),
+        expected_double_2(),
+    ];
+
+    assert_eq!(actual.len(), expected.len());
+    assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual[1], expected[1]);
+}
 
 #[test]
 #[ignore]
 fn double_pdf() {
     let actual = get_qr_bill_data("tests/data/double.pdf".to_string(), true);
-
     let expected = vec![
-        QRData::new(
-            "CH3389144927977473182".to_string(),
-            Address::new(
-                "S".to_string(),
-                "Test Recipient AG".to_string(),
-                "Teststreet 42a".to_string(),
-                "9000 Zürich".to_string(),
-                "CH".to_string(),
-            ),
-            Some(Address::new(
-                "S".to_string(),
-                "Sender AG".to_string(),
-                "Senderstreet 99C".to_string(),
-                "1234 Sendertown".to_string(),
-                "AT".to_string(),
-            )),
-            Some("1337.42".to_string()),
-            "EUR".to_string(),
-            "SCOR".to_string(),
-            Some("RF541234".to_string()),
-            Some("This is a test Message".to_string()),
-        ),
-        QRData::new(
-            "CH4289144165265158476".to_string(),
-            Address::new(
-                "S".to_string(),
-                "A".to_string(),
-                " ".to_string(),
-                "8000 Zürich".to_string(),
-                "CH".to_string(),
-            ),
-            None,
-            None,
-            "CHF".to_string(),
-            "NON".to_string(),
-            None,
-            None,
-        ),
+        expected_double_1(),
+        expected_double_2(),
     ];
 
     assert_eq!(actual.len(), expected.len());
     assert_eq!(actual[0], expected[0]);
+    assert_eq!(actual[1], expected[1]);
 }
 
 #[test]
@@ -287,9 +165,139 @@ fn none_png() {
 }
 
 #[test]
+fn none_jpg() {
+    let actual = get_qr_bill_data("tests/data/none.jpg".to_string(), true);
+
+    assert!(actual.is_empty());
+}
+
+
+#[test]
+fn none_jpeg() {
+    let actual = get_qr_bill_data("tests/data/none.jpeg".to_string(), true);
+
+    assert!(actual.is_empty());
+}
+
+
+#[test]
 #[ignore]
 fn none_pdf() {
     let actual = get_qr_bill_data("tests/data/none.pdf".to_string(), true);
 
     assert!(actual.is_empty());
 }
+
+pub fn expected_minimal() -> QRData {
+    QRData::new(
+        "CH4289144165265158476".to_string(),
+        Address::new(
+            "S".to_string(),
+            "A".to_string(),
+            " ".to_string(),
+            "8000 Zürich".to_string(),
+            "CH".to_string(),
+        ),
+        None,
+        None,
+        "CHF".to_string(),
+        "NON".to_string(),
+        None,
+        None,
+    )
+}
+
+pub fn expected_full() -> QRData {
+    QRData::new(
+        "CH3389144927977473182".to_string(),
+        Address::new(
+            "S".to_string(),
+            "Test Recipient AG".to_string(),
+            "Teststreet 42a".to_string(),
+            "9000 Zürich".to_string(),
+            "CH".to_string(),
+        ),
+        Some(Address::new(
+            "S".to_string(),
+            "Sender AG".to_string(),
+            "Senderstreet 99C".to_string(),
+            "1234 Sendertown".to_string(),
+            "AT".to_string(),
+        )),
+        Some("1337.42".to_string()),
+        "EUR".to_string(),
+        "SCOR".to_string(),
+        Some("RF541234".to_string()),
+        Some("This is a test Message".to_string()),
+    )
+}
+
+pub fn expected_rotated() -> QRData {
+    QRData::new(
+        "CH3389144927977473182".to_string(),
+        Address::new(
+            "S".to_string(),
+            "Test Recipient AG".to_string(),
+            "Teststreet 42a".to_string(),
+            "9000 Zürich".to_string(),
+            "CH".to_string(),
+        ),
+        Some(Address::new(
+            "S".to_string(),
+            "Sender AG".to_string(),
+            "Senderstreet 99C".to_string(),
+            "1234 Sendertown".to_string(),
+            "AT".to_string(),
+        )),
+        Some("1337.42".to_string()),
+        "EUR".to_string(),
+        "SCOR".to_string(),
+        Some("RF541234".to_string()),
+        Some("This is a test Message".to_string()),
+    )
+}
+
+pub fn expected_double_1() -> QRData {
+    QRData::new(
+        "CH3389144927977473182".to_string(),
+        Address::new(
+            "S".to_string(),
+            "Test Recipient AG".to_string(),
+            "Teststreet 42a".to_string(),
+            "9000 Zürich".to_string(),
+            "CH".to_string(),
+        ),
+        Some(Address::new(
+            "S".to_string(),
+            "Sender AG".to_string(),
+            "Senderstreet 99C".to_string(),
+            "1234 Sendertown".to_string(),
+            "AT".to_string(),
+        )),
+        Some("1337.42".to_string()),
+        "EUR".to_string(),
+        "SCOR".to_string(),
+        Some("RF541234".to_string()),
+        Some("This is a test Message".to_string()),
+    )
+}
+
+pub fn expected_double_2() -> QRData {
+    QRData::new(
+        "CH4289144165265158476".to_string(),
+        Address::new(
+            "S".to_string(),
+            "A".to_string(),
+            " ".to_string(),
+            "8000 Zürich".to_string(),
+            "CH".to_string(),
+        ),
+        None,
+        None,
+        "CHF".to_string(),
+        "NON".to_string(),
+        None,
+        None,
+    )
+}
+
